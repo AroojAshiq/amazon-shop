38d68c8612f6985c378fee8444c663d952fcd783
Arooj Ashiq <ashiqarooj846@example.com>
2022-12-16 12:27:01 +0500

    deployment

---
+++ frontend/build/precache-manifest.3d9940ae76540930945fd53a31fded0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ac4f4f605ae560fa347f88302b3d49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feb68d23567d96447ea3",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb68d23567d96447ea3",
+    "url": "/static/js/main.d29c4346.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
